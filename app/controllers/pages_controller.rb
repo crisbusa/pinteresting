214555fa64c34a3a0860372aad8c33a671d7e813
crisrtobal Bugeda <cristobalbugeda@gmail.com>
2014-12-09 20:15:58 -0600

    Canceled music page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,5 @@ class PagesController < ApplicationController
   end
   def contact
   end
-  def music
-  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
  get "contact" => "pages#contact"
 
 
- get "music" => "pages#music"
-
  
 
 
