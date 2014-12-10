b9680c132fc71635506d84617bf651a1b12cdf33
crisrtobal Bugeda <cristobalbugeda@gmail.com>
2014-12-09 19:50:38 -0600

    initial pages (home, about contact music

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
+ root "pages#home"
+
+
+ get "about" => "pages#about"
+
+ 
+ get "contact" => "pages#contact"
+
+
+ get "music" => "pages#music"
+
+ 
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
